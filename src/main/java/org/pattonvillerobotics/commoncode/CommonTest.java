c5b8a1ad602d8830e79a794d3806688b83897537
magneticflux- <skaggsm333@gmail.com>
2016-05-10 16:25:23 -0500

    Added cross-module test

--- src/main/java/org/pattonvillerobotics/commoncode/CommonTest.java
+++ src/main/java/org/pattonvillerobotics/commoncode/CommonTest.java
@@ -1,7 +1,13 @@
 package org.pattonvillerobotics.commoncode;
 
+import android.util.Log;
+
 /**
  * Created by skaggsm on 4/27/16.
  */
 public class CommonTest {
+
+    public static void test() {
+        Log.e(CommonTest.class.getName(), "Code executed!");
+    }
 }
