1f2bd0ffe35ca75b4f82061306af9e726d3e453f
Mitchell Skaggs <skaggsm333@gmail.com>
2016-09-23 07:44:24 -0500

    Added fluent builder for RobotParameters, defaults included

--- src/main/java/org/pattonvillerobotics/commoncode/robotclasses/RobotParameters.java
+++ src/main/java/org/pattonvillerobotics/commoncode/robotclasses/RobotParameters.java
@@ -5,6 +5,80 @@ package org.pattonvillerobotics.commoncode.robotclasses;
  */
 
 public class RobotParameters {
-    private double wheelBaseRadius, wheelRadius, driveGearRatio;
-    private boolean gyroEnabled, encodersEnabled;
+    public static final int TICKS_PER_REVOLUTION = 1440;
+
+    private final double wheelBaseRadius, wheelRadius, driveGearRatio;
+    private final boolean gyroEnabled, encodersEnabled;
+
+    private RobotParameters(double wheelBaseRadius, double wheelRadius, double driveGearRatio, boolean gyroEnabled, boolean encodersEnabled) {
+        this.wheelBaseRadius = wheelBaseRadius;
+        this.wheelRadius = wheelRadius;
+        this.driveGearRatio = driveGearRatio;
+        this.gyroEnabled = gyroEnabled;
+        this.encodersEnabled = encodersEnabled;
+    }
+
+    public double getWheelBaseRadius() {
+        return wheelBaseRadius;
+    }
+
+    public double getWheelRadius() {
+        return wheelRadius;
+    }
+
+    public double getDriveGearRatio() {
+        return driveGearRatio;
+    }
+
+    public boolean isGyroEnabled() {
+        return gyroEnabled;
+    }
+
+    public boolean isEncodersEnabled() {
+        return encodersEnabled;
+    }
+
+    public class Builder {
+        private double wheelBaseRadius;
+        private double wheelRadius;
+        private double driveGearRatio = 1;
+        private boolean gyroEnabled = false;
+        private boolean encodersEnabled = false;
+
+        public Builder() {
+        }
+
+        public Builder wheelBaseRadius(double wheelBaseRadius) {
+            this.wheelBaseRadius = wheelBaseRadius;
+            return this;
+        }
+
+        public Builder wheelRadius(double wheelRadius) {
+            this.wheelRadius = wheelRadius;
+            return this;
+        }
+
+        public Builder driveGearRatio(double driveGearRatio) {
+            this.driveGearRatio = driveGearRatio;
+            return this;
+        }
+
+        public Builder gyroEnabled(boolean gyroEnabled) {
+            this.gyroEnabled = gyroEnabled;
+            return this;
+        }
+
+        public Builder encodersEnabled(boolean encodersEnabled) {
+            this.encodersEnabled = encodersEnabled;
+            return this;
+        }
+
+        public RobotParameters build() {
+            if (wheelBaseRadius <= 0)
+                throw new IllegalArgumentException("wheelBaseRadius must be > 0");
+            if (wheelRadius <= 0)
+                throw new IllegalArgumentException("wheelRadius must be > 0");
+            return new RobotParameters(wheelBaseRadius, wheelRadius, driveGearRatio, gyroEnabled, encodersEnabled);
+        }
+    }
 }
