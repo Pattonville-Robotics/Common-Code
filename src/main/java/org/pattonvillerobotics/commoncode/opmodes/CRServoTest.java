44481bfee61156d0b40952246add9d798d5be301
greg-bahr <greg.bahr123@gmail.com>
2017-03-16 18:31:25 -0500

    created a test opmode for CR Servos

---
+++ src/main/java/org/pattonvillerobotics/commoncode/opmodes/CRServoTest.java
@@ -0,0 +1,51 @@
+package org.pattonvillerobotics.commoncode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.GamepadData;
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableButton;
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableGamepad;
+
+/**
+ * Created by bahrg on 3/16/17.
+ */
+@TeleOp(name = "CRServoTest")
+public class CRServoTest extends LinearOpMode {
+
+    private CRServo crServo;
+    private ListenableGamepad gamepad;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        crServo = hardwareMap.crservo.get("crservo");
+        gamepad = new ListenableGamepad();
+        gamepad.getButton(GamepadData.Button.A).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
+            @Override
+            public void run() {
+                crServo.setPower(1);
+            }
+        });
+
+        gamepad.getButton(GamepadData.Button.B).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
+            @Override
+            public void run() {
+                crServo.setPower(0);
+            }
+        });
+
+        gamepad.getButton(GamepadData.Button.X).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
+            @Override
+            public void run() {
+                crServo.setPower(-1);
+            }
+        });
+        waitForStart();
+
+        while (opModeIsActive()) {
+            gamepad.update(new GamepadData(gamepad1));
+            idle();
+        }
+    }
+}
