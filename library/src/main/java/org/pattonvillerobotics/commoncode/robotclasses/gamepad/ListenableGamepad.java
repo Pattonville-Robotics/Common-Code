bee6475bece0a6740aacaa89591cafb7a0a74eb2
Mitchell Skaggs <skaggsm333@gmail.com>
2017-10-21 16:01:48 -0500

    Added documentation to ListenableGamepad and GamepadData

--- library/src/main/java/org/pattonvillerobotics/commoncode/robotclasses/gamepad/ListenableGamepad.java
+++ library/src/main/java/org/pattonvillerobotics/commoncode/robotclasses/gamepad/ListenableGamepad.java
@@ -1,16 +1,24 @@
 package org.pattonvillerobotics.commoncode.robotclasses.gamepad;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by skaggsm on 10/6/16.
+ * This class allows users to subscribe to certain button events in an event-driven fashion.
+ *
+ * @author Mitchell Skaggs
+ * @since 3.4.0
  */
 
 public class ListenableGamepad {
 
     private final Map<GamepadData.Button, ListenableButton> buttons;
 
+    /**
+     * Constructs a new ListenableGamepad and populates it with {@link ListenableButton} instances
+     */
     public ListenableGamepad() {
         this.buttons = new HashMap<>();
 
@@ -18,11 +26,32 @@ public class ListenableGamepad {
             buttons.put(b, new ListenableButton());
     }
 
+    /**
+     * Calls all button listeners based on the new gamepad values.
+     *
+     * @param gamepadData the gamepad values
+     */
     public void update(GamepadData gamepadData) {
         for (GamepadData.Button b : GamepadData.Button.values())
             buttons.get(b).update(gamepadData.getButtonPressed(b));
     }
 
+
+    /**
+     * Calls all button listeners based on the new gamepad values.
+     *
+     * @param gamepad the gamepad from which to extract immutable values
+     */
+    public void update(Gamepad gamepad) {
+        this.update(new GamepadData(gamepad));
+    }
+
+    /**
+     * Gets an instance of a button from the button's corresponding enum
+     *
+     * @param button the button enum to find
+     * @return a button matching the enum
+     */
     public ListenableButton getButton(GamepadData.Button button) {
         return buttons.get(button);
     }
