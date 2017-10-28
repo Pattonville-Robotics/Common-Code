6ff1b673239a66e264ce13801b8af6c22401a5a3
Mitchell Skaggs <skaggsm333@gmail.com>
2017-10-28 10:41:31 -0500

    Added Java 8 feature support

--- library/src/main/java/org/pattonvillerobotics/commoncode/opmodes/CRServoTest.java
+++ library/src/main/java/org/pattonvillerobotics/commoncode/opmodes/CRServoTest.java
@@ -23,31 +23,15 @@ public class CRServoTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         crServo = hardwareMap.crservo.get("crservo");
         gamepad = new ListenableGamepad();
-        gamepad.getButton(GamepadData.Button.A).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
-            @Override
-            public void run() {
-                crServo.setPower(1);
-            }
-        });
+        gamepad.getButton(GamepadData.Button.A).addListener(ListenableButton.ButtonState.JUST_PRESSED, () -> crServo.setPower(1));
 
-        gamepad.getButton(GamepadData.Button.B).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
-            @Override
-            public void run() {
-                crServo.setPower(0);
-            }
-        });
+        gamepad.getButton(GamepadData.Button.B).addListener(ListenableButton.ButtonState.JUST_PRESSED, () -> crServo.setPower(0));
 
-        gamepad.getButton(GamepadData.Button.X).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
-            @Override
-            public void run() {
-                crServo.setPower(-1);
-            }
-        });
+        gamepad.getButton(GamepadData.Button.X).addListener(ListenableButton.ButtonState.JUST_PRESSED, () -> crServo.setPower(-1));
         waitForStart();
 
         while (opModeIsActive()) {
-            gamepad.update(new GamepadData(gamepad1));
-            idle();
+            gamepad.update(gamepad1);
         }
     }
 }
